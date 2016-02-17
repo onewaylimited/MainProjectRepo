f029895eff58478b8d34ec0ae6b2ef09a59f950f
Ryan Crotty <rtcrotty@mtu.edu>
2016-02-17 16:55:14 -0500

    Fixing... nothing?
    Visual Studio was yelling at me for inconsistent line feeds, so this
    *should* fix that.

--- Super Broomball 2016/Assets/SpinThisThing.cs
+++ Super Broomball 2016/Assets/SpinThisThing.cs
@@ -10,6 +10,6 @@ public class SpinThisThing : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        transform.Rotate(0, 0, 1);
+        transform.Rotate(0, 0, 1);  
 	}
 }
