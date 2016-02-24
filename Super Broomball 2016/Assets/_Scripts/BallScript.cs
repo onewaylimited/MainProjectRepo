1ae0940147e6509b06cfec94776e034ff0b1fb5a
dkwagner <dkwagner@mtu.edu>
2016-02-24 15:42:03 -0500

    Audio!!!
    Added a crappy impact sound for the ball on one of the walls

--- Super Broomball 2016/Assets/_Scripts/BallScript.cs
+++ Super Broomball 2016/Assets/_Scripts/BallScript.cs
@@ -3,9 +3,11 @@ using System.Collections;
 
 public class BallScript : MonoBehaviour {
 
+    private AudioSource source;
+
 	// Use this for initialization
 	void Start () {
-	
+        source = GetComponent<AudioSource>();
 	}
 	
 	// Update is called once per frame
--- Super Broomball 2016/Assets/_Scripts/RinkScript.cs
+++ Super Broomball 2016/Assets/_Scripts/RinkScript.cs
@@ -5,9 +5,11 @@ public class RinkScript : MonoBehaviour {
 
     public bool leftTeam;
 
+    private AudioSource audio;
+
 	// Use this for initialization
 	void Start () {
-	
+        audio = GetComponent<AudioSource>();
 	}
 	
 	// Update is called once per frame
@@ -17,5 +19,11 @@ public class RinkScript : MonoBehaviour {
 
     void OnTriggerEnter2D(Collider2D coll) {
         if (coll.gameObject.tag == "ball") print(leftTeam?"Right team scored!":"Left team scored!");
+    }
+
+    void OnCollisionEnter2D(Collision2D coll) {
+        if(coll.gameObject.tag == "ball") {
+            audio.Play();
+        }
     }
 }
