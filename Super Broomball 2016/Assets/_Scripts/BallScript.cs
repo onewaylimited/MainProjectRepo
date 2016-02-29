0f96a90c4fe9dd7c2e1f7ed1b84f2c0fe2dfc2bb
Ryan Crotty <rtcrotty@mtu.edu>
2016-02-29 11:29:28 -0500

    Added basic code to reset ball to initial position when a goal is scored

--- Super Broomball 2016/Assets/_Scripts/BallScript.cs
+++ Super Broomball 2016/Assets/_Scripts/BallScript.cs
@@ -7,14 +7,20 @@ public class BallScript : MonoBehaviour {
     Rigidbody2D player;
     public bool inPossession;
 
-
+    private Vector3 startPos;
     private AudioSource source;
 
 
 	// Use this for initialization
 	void Start () {
+        startPos = transform.position;
         source = GetComponent<AudioSource>();
 	}
+
+    public void returnToCenter() {
+        transform.position = startPos;
+        GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+    }
 	
 	// Update is called once per frame
 	void Update () {
