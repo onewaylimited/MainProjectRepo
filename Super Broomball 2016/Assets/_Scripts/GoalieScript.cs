bb08dbf91b4dd5946af0091c671157d3ee4f29e9
James Lutey <jace.lutey@gmail.com>
2016-03-20 17:24:06 -0400

    Goalie
    Added a rudimentary goalie that can move up and down when the ball is
    near in an attempt to block it.

---
+++ Super Broomball 2016/Assets/_Scripts/GoalieScript.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GoalieScript : MonoBehaviour
+{
+
+    public GameObject ball = null;
+    private GameObject player = null;
+    private bool dangerZone = false;
+    // Use this for initialization
+    void Start()
+    {
+        player = this.gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (dangerZone)
+        {
+            Block();
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D coll)
+    {
+
+        if (coll.gameObject.tag == "ball")
+        {
+            //ball is nearby
+            ball = coll.gameObject;
+            dangerZone = true;
+        }
+    }
+
+
+    void OnTriggerExit2D(Collider2D coll)
+    {
+        if (coll.gameObject.tag == "ball")
+        {
+            ball = null;
+            dangerZone = false;
+        }
+    }
+    void Block()
+    {
+        if (ball.GetComponent<Transform>().position.y > player.GetComponent<Transform>().position. y)
+        {
+            player.GetComponent<Rigidbody2D>().AddForce(new Vector3(0, 5, 0));
+        }
+        else if (ball.GetComponent<Transform>().position.y < player.GetComponent<Transform>().position.y)
+        {
+            player.GetComponent<Rigidbody2D>().AddForce(new Vector3(0, -5, 0));
+        }
+    }
+}
